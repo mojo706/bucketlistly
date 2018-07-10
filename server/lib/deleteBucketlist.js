1df55652177ad209f9b6dbcdb951b2539e30beb4
basedHokage <eugene.omar@andela.com>
2018-07-10 17:53:48 +0300

    [ft-add-lib-files] add lib method to delete bucketlists

---
+++ server/lib/deleteBucketlist.js
@@ -0,0 +1,26 @@
+const models = require('../models/index')
+
+/**
+ * @function deleteBucketlist
+ * deletes bucketlist
+ * @param {String} id
+ * @return {Object} bucketlist
+ */
+module.exports = async id => {
+  try {
+    let errorMessage = ''
+    let successMessage = ''
+    const bucketlistToDelete = await models.Bucketlist.findById(id)
+    if (!bucketlistToDelete) {
+      errorMessage = 'That Buckelist Does not exist'
+      return { error: errorMessage }
+    }
+    await models.Bucketlist.destroy({
+      where: { id },
+    })
+    successMessage = 'Bucketlist successfully deleted'
+    return successMessage
+  } catch (err) {
+    throw new Error(err)
+  }
+}
