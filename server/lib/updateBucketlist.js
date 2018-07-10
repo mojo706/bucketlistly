0a123927d133df8c923cc61a25fab39e7760ea8f
basedHokage <eugene.omar@andela.com>
2018-07-10 17:55:22 +0300

    [ft-add-lib-files] add lib method to update a bucketlist

---
+++ server/lib/updateBucketlist.js
@@ -0,0 +1,24 @@
+const models = require('../models/index')
+
+/**
+ * @function updateBucketlist
+ * updates a bucketlist
+ * @param {Object} body
+ * @return {Object} bucketlist
+ */
+module.exports = async (id, body) => {
+  try {
+    let errorMessage = ''
+    const bucketlistToUpdate = await models.Bucketlist.findById(id)
+    if (!bucketlistToUpdate) {
+      errorMessage = 'That Buckelist Does not exist'
+      return { error: errorMessage }
+    }
+    const updatedBucketlist = await models.Bucketlist.update(body, {
+      where: { id },
+    })
+    return { bucketlist: updatedBucketlist }
+  } catch (err) {
+    throw new Error(err)
+  }
+}
