52ac9e9e2744a5cc76852d3622ca0dfc36d34a01
basedHokage <eugene.omar@andela.com>
2018-10-03 16:00:40 +0300

    [ft-verify-user-email] refactor code to add a better method of adding ids

--- server/models/verificationToken.js
+++ server/models/verificationToken.js
@@ -1,9 +1,17 @@
+const cuid = require('cuid')
+
 module.exports = (sequelize, DataTypes) => {
   const VerificationToken = sequelize.define(
     'VerificationTokens',
     {
+      id: {
+        primaryKey: true,
+        type: DataTypes.STRING,
+        defaultValue: () => cuid(),
+      },
       userId: DataTypes.STRING,
       token: DataTypes.STRING,
+      expireToken: DataTypes.STRING,
     },
     {
       classMethods: {
